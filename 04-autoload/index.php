6666c0b116e10fb092f352308216d236e4c1dd62
Fábio Santos <fabio3268@gmail.com>
2022-06-08 14:36:07 -0300

    Até Autoload

---
+++ 04-autoload/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/source/autoload.php";
+
+$apnp = new \Source\Students\Apnp\Student();
+$presential = new \Source\Students\Presential\Student();
+
+var_dump($apnp, $presential);
