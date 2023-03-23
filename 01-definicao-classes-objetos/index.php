0f61d2b202f567ee87333a36d1f25fe5975a03f7
Fábio Santos <fabio3268@gmail.com>
2023-03-23 15:56:25 -0300

    Definição de Classes e Objtos

--- 01-definicao-classes-objetos/index.php
+++ 01-definicao-classes-objetos/index.php
@@ -1,3 +1,10 @@
 <?php
-
-
+echo __DIR__;
+require __DIR__ . "/../source/User.php";
+$user = new User();
+$user->setName("Fábio");
+$user->setEmail("fabiosantos@ifsul.edu.br");
+var_dump($user);
+echo "Seu nome é: {$user->getName()} - seu e-mail é: {$user->getEmail()}";
+$user02 = new User("Fábio","fabio@gmail.com");
+var_dump($user02);
--- source/User.php
+++ source/User.php
@@ -1,3 +1,32 @@
 <?php
 
+class User {
+    private $name;
+    private $email;
 
+    public function __construct ($name = null,
+                                 $email = null){
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+}
